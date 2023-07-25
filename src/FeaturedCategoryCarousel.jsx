43b9a790a24f57f15309f29869bb61b4cf852ecb
Sharjeel-22 <pcraboot55441@gmail.com>
2023-07-26 02:10:44 +0500

    worked on add second carousel

--- src/FeaturedCategoryCarousel.jsx
+++ src/FeaturedCategoryCarousel.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import FeaturedCategory from "./FeaturedCategories";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 export default function FeaturedCategoryCarousel() {
   const onChange = (selectedIndex, selectedSlide) => {
@@ -15,63 +18,116 @@ export default function FeaturedCategoryCarousel() {
   const onClickThumb = (index, item) => {
     // Handle onClickThumb event here if needed
   };
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
 
   return (
-    <div className="carousel-wrapper">
-      <div className="carousel-row flex">
-        <div className="carousel-content flex justify-center">
-          <Carousel
-            autoPlay
-            width={650}
-            onChange={onChange}
-            onClickItem={onClickItem}
-            onClickThumb={onClickThumb}
-          >
-            <div className="images-wrapper">
-              <img
-                className="images"
-                src="https://img.freepik.com/free-vector/desktop-computer-concept-illustration_114360-13052.jpg?w=1380&t=st=1690236470~exp=1690237070~hmac=50bb44b9c9cb67ab07bc714582d4ab48ba391c1da8e4e805a74557dc80f99a54"
-                alt="Slide 1"
-              />
-            </div>
-            <div className="images-wrapper">
-              <img
-                className="images"
-                src="https://img.freepik.com/free-vector/professional-desk-with-isometric-perspective_23-2147687154.jpg?w=826&t=st=1690236491~exp=1690237091~hmac=81175f1f3a6d082e959272e70cd310f0c570397f4efdf97ab80ea287bb7df391"
-                alt="Slide 2"
-              />
-            </div>
-            <div className="images-wrapper">
-              <img
-                className="images"
-                src="https://img.freepik.com/free-vector/modern-desktop-compute-concept-illustration_114360-12156.jpg?w=1380&t=st=1690236400~exp=1690237000~hmac=a2be1fbf6cf128aea17f24307ce51b207e5a1322ab680ce873f8dfaad4094088"
-                alt="Slide 3"
-              />
-            </div>
-            <div className="images-wrapper">
-              <img
-                className="images"
-                src="https://img.freepik.com/free-vector/modern-desktop-computer-concept-illustration_114360-11616.jpg?w=826&t=st=1690236428~exp=1690237028~hmac=aa4c92b2d8da4fc5dfe6f11f2a4382c7b7f80cd652bb4088b7fd922fa35527b6"
-                alt="Slide 4"
-              />
-            </div>
-            <div className="images-wrapper">
-              <img
-                className="images"
-                src="https://img.freepik.com/free-vector/modern-desktop-computer-concept-illustration_114360-16235.jpg?w=1380&t=st=1690236445~exp=1690237045~hmac=60d76f2607c1d04cf31338ac3b650ca6579223a00ff7cd90f3589cfb3f19325c"
-                alt="Slide 5"
-              />
-            </div>
-            <div className="images-wrapper">
-              <img
-                className="images"
-                src="https://img.freepik.com/free-vector/desktop-computer-concept-illustration_114360-13052.jpg?w=1380&t=st=1690236470~exp=1690237070~hmac=50bb44b9c9cb67ab07bc714582d4ab48ba391c1da8e4e805a74557dc80f99a54"
-                alt="Slide 6"
-              />
-            </div>
-          </Carousel>
+    <div className="featured-category-main">
+      <Slider {...settings} autoplay>
+        <div className="box">
+          <a href="https://www.fepy.com/home-improvement">
+            <img
+              className="swiper"
+              src="https://ik.imagekit.io/fepy/cdn/catalog/category/thumbnail/Cater-05.png"
+              alt="Home &amp; Furniture"
+              width="250"
+              height="250"
+            />
+          </a>
+          <div className="cate-name">
+            <p>Home &amp; Furniture</p>
+          </div>
         </div>
-      </div>
+        <div className="box">
+          <a href="https://www.fepy.com/building-material/construction-chemical-materials">
+            <img
+              className="swiper"
+              src="https://ik.imagekit.io/fepy/cdn/catalog/category/thumbnail/Cater-07.png"
+              alt="Construction Chemical"
+              width="250"
+              height="250"
+            />
+          </a>
+          <div className="cate-name">
+            <p>Construction Chemical</p>
+          </div>
+        </div>
+        <div className="box">
+          <a href="https://www.fepy.com/building-material/gypsum">
+            <img
+              className="swiper"
+              src="https://ik.imagekit.io/fepy/cdn/catalog/category/thumbnail/Cater-08.png"
+              alt="Gypsum"
+              width="250"
+              height="250"
+            />
+          </a>
+          <div className="cate-name">
+            <p>Gypsum</p>
+          </div>
+          <div className="box">
+          <a href="https://www.fepy.com/electrical-lightning">
+            <img
+              className="swiper"
+              src="https://ik.imagekit.io/fepy/cdn/catalog/category/thumbnail/Cater-09.png"
+              alt="Electrical &amp; Lighting"
+              width="250"
+              height="250"
+            />
+          </a>
+          <div className="cate-name">
+            <p>Electrical &amp; Lighting</p>
+          </div>
+        </div>
+        <div className="box">
+          <a href="https://www.fepy.com/plumbing">
+            <img
+              className="swiper"
+              src="https://ik.imagekit.io/fepy/cdn/catalog/category/thumbnail/Cater-10.png"
+              alt="Plumbing"
+              width="250"
+              height="250"
+            />
+          </a>
+          <div className="cate-name">
+            <p>Plumbing</p>
+          </div>
+        </div>
+        </div>
+
+      </Slider>
     </div>
   );
 }
